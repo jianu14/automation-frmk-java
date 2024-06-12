54f43c291c42ea44143f0e4811efabeae465125c
jianu.iancu <jianu.iancu@fanduel.com>
2024-06-12 13:25:57 +0300

    optimised imports in DriverFactory.java

--- src/test/java/com/qacart/todo/factory/DriverFactory.java
+++ src/test/java/com/qacart/todo/factory/DriverFactory.java
@@ -1,7 +1,6 @@
 package com.qacart.todo.factory;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.groovy.json.internal.Chr;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
